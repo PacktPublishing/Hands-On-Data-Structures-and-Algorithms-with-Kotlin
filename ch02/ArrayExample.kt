cf5c60fa51a34295c2c89b418e1d24bc762bf196
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2018-04-16 23:14:49 +0530

    1st example for 2nd chapter.

---
+++ ch02/ArrayExample.kt
@@ -0,0 +1,6 @@
+import java.util.Arrays
+
+fun main(args: Array<String>) {
+    val friends = arrayOf("Rivu", "Subin", "Sid", "Susri", "Ramya", "Sachin")
+    println("My friends are - ${Arrays.toString(friends)}")
+}
