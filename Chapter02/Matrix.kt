0f8515a0c1385861cdd5c9cf7fe369d0a3ca43c6
Nayak <nayakc2@medtronic.com>
2018-05-02 19:06:07 +0530

    Added one more example in Matrix.kt file.

--- Chapter02/Matrix.kt
+++ Chapter02/Matrix.kt
@@ -25,4 +25,7 @@ fun main(args: Array<String>) {
         arrayOf(3, 4, 3)
     )
     println("${Arrays.deepToString(matrix1)} is ${if (isValidMatrix(matrix1)) "a valid" else "an invalid"} Matrix")
+
+    val matrix2 = arrayOf(1, 3, 6)
+    println("${Arrays.toString(matrix2)} is ${if (matrix2 is Array<*>) "a valid" else "an invalid"} Matrix")
 }
