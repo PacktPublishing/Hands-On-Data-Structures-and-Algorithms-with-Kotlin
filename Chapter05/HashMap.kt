367c44fcf95b60265b7aa1805cb20c326f349797
Nayak <nayakc2@medtronic.com>
2018-06-21 19:22:52 +0530

    Implemented constructor for HashMap.

--- Chapter05/HashMap.kt
+++ Chapter05/HashMap.kt
@@ -3,12 +3,26 @@ import kotlin.math.*
 
 class HashMap<K, V> {
 
-    val minCapacity = 4
+    private val minCapacity = 4
+    private val maxCapacity = 1 shl 30
 
-    var table: Node<K, V>[]
+    private var table: Array<Node<K, V>?>
 
     constructor() {
-        table = arrayOfNulls(minCapacity / 2)
+        this.table = arrayOfNulls(minCapacity)
+    }
+
+    constructor(capacity: Int) {
+        val finalCapacity: Int
+        if (capacity < 0) throw IllegalArgumentException("Invalid Capacity: " + capacity);
+        if (capacity < minCapacity) finalCapacity = minCapacity
+        else if (capacity > maxCapacity) finalCapacity = maxCapacity
+        else finalCapacity = fetchNearestCapacity(capacity)
+        this.table = makeTable(finalCapacity)
+    }
+
+    private fun makeTable(capacity: Int): Array<Node<K, V>?> {
+        return arrayOfNulls(capacity)
     }
 
     class Node<K, V>(
@@ -26,5 +40,18 @@ class HashMap<K, V> {
             if (other is Node<*, *> && this.key == other.key && this.value == other.value) return true
             return false
         } 
+    }
+
+    private fun fetchNearestCapacity(i: Int): Int {
+        var retVal = i - 1 // If input is a power of two, shift its high-order bit right.
+
+        // "Smear" the high-order bit all the way to the right.
+        retVal = retVal or retVal ushr 1
+        retVal = retVal or retVal ushr 2
+        retVal = retVal or retVal ushr 4
+        retVal = retVal or retVal ushr 8
+        retVal = retVal or retVal ushr 16
+
+        return retVal + 1
     }
 }
