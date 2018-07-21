190f49c0737ea628c108a8bb1387228abea67ff7
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2018-07-21 16:10:11 +0530

    Pattern searching in brute force way is implemented.

---
+++ Chapter06/NaivePatternSearch.kt
@@ -0,0 +1,29 @@
+fun search(text: String, pattern: String): Int {
+    var retVal = -1
+    val patternLen = pattern.length
+    val textLen = text.length - patternLen
+    for (i in 0 until textLen) {
+        var isFound = true
+        for (j in 0 until patternLen) {
+            if (text[i + j] != pattern[j]) {
+                isFound = false
+                break
+            }
+        }
+        if (isFound) {
+            retVal = i
+            break
+        }
+    }
+    return retVal
+}
+
+fun main(args: Array<String>) {
+    println(search("Hello World!!", "abc"))
+    println(search("Hello World!!", "Hel"))
+    println(search("Hello World!!", "elo"))
+    println(search("Hello World!!", "el"))
+    println(search("Hello World!!", "wo"))
+    println(search("Hello World!!", "Wo"))
+    println(search("Hello World!!", "Wod"))
+}
