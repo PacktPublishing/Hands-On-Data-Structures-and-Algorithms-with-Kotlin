cc9124ce1584b6c41c78a0288631be17229e32f6
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2018-08-16 23:27:54 +0530

    Implemented HashMap

--- Chapter05/HashMap.kt
+++ Chapter05/HashMap.kt
@@ -3,33 +3,133 @@ import kotlin.math.*
 
 class HashMap<K, V> {
 
-    private val minCapacity = 4
+    private val minCapacity = 1 shl 4
     private val maxCapacity = 1 shl 30
+    private val loadFactor = 0.75f
 
     private var table: Array<Node<K, V>?>
+    private var size = 0
 
     constructor() {
         this.table = arrayOfNulls(minCapacity)
     }
 
     constructor(capacity: Int) {
-        val finalCapacity: Int
-        if (capacity < 0) throw IllegalArgumentException("Invalid Capacity: " + capacity);
-        if (capacity < minCapacity) finalCapacity = minCapacity
-        else if (capacity > maxCapacity) finalCapacity = maxCapacity
-        else finalCapacity = fetchNearestCapacity(capacity)
+        if (capacity < 0) throw IllegalArgumentException("Invalid Capacity: $capacity")
+        val finalCapacity = when {
+            capacity < minCapacity -> minCapacity
+            capacity > maxCapacity -> maxCapacity
+            else -> fetchNearestCapacity(capacity)
+        }
         this.table = makeTable(finalCapacity)
+    }
+
+    private fun hash(key: K): Int {
+        val h = key?.hashCode() ?: 0
+        return h xor (h ushr 16)
     }
 
     private fun makeTable(capacity: Int): Array<Node<K, V>?> {
         return arrayOfNulls(capacity)
     }
 
+    fun isEmpty() = size == 0
+
+    fun get(key: K): V? {
+        val e = getNode(hash(key), key)
+        return if (e == null) null else e.value
+    }
+
+    private fun getNode(hash: Int, key: K): Node<K, V>? {
+        val n = table.size
+        if (n > 0) {
+            val first = table[(n - 1) and hash]
+            if (first != null) {
+                if (first.hash == hash) { // Checking the 1st node
+                    val k = first.key
+                    if (k === key || k == key) return first
+                }
+                var e = first.next ?: return null
+                do {
+                    if (e.hash == hash && e.key === key || e.key == key) return e
+                } while (e.next != null)
+            }
+        }
+        return null
+    }
+
+    fun containsKey(key: K) = getNode(hash(key), key) != null
+
+    fun put(key: K, value: V) {
+        putVal(key, value)
+    }
+
+    fun putVal(key: K, value: V, onlyIfAbsent: Boolean = false) {
+        val hash = hash(key)
+        val n = table.size
+        val index = (n - 1) and hash
+        var first = table[index]
+        if (first == null) {
+            table[index] = Node(hash, key, value, null)
+        } else {
+            var node: Node<K, V>?
+            var k = first.key
+            if (first.hash == hash && (k === key || k == key) && !onlyIfAbsent) first.value = value
+            else {
+                while(true) {
+                    node = first!!.next
+                    if (node == null) {
+                        first.next = Node(hash, key, value, null)
+                        break
+                    }
+                    k = node.key
+                    if (node.hash == hash && (k === key || k == key) && !onlyIfAbsent) {
+                        node.value = value
+                        break
+                    }
+                    first = node
+                }
+            }
+        }
+    }
+
+    fun remove(key: K): V? {
+        val hash = hash(key)
+        val n = table.size
+        val index = (n - 1) and hash
+        var first = table[index]
+        if (n > 0 && first != null) {
+            var node: Node<K, V>? = null
+            var k = first.key
+            if (first.hash == hash && (key === k || key == k)) node = first
+            else {
+                var nextNode = first.next
+                if (nextNode != null) {
+                    do {
+                        k = nextNode!!.key
+                        if (nextNode!!.hash == hash && (key === k || key == k)) {
+                            node = nextNode
+                            break
+                        }
+                        first = nextNode
+                        nextNode = nextNode!!.next
+                    } while(nextNode != null)
+                }
+            }
+            if (node != null) {
+                if (node == first) table[index] = node.next
+                else first!!.next = node.next
+                return node.value
+            }
+        }
+        return null
+    }
+
     class Node<K, V>(
         val hash: Int,
         val key: K,
         var value: V,
-        var next: Node<K, V>) {
+        var next: Node<K, V>?) {
 
         override fun toString() = "$key=$value"
 
@@ -54,4 +154,27 @@ class HashMap<K, V> {
 
         return retVal + 1
     }
+}
+
+fun main(args: Array<String>) {
+    val map = HashMap<String, String>()
+    println(map.get("Test"))
+    map.put("Test", "Testing")
+    println(map.get("Test"))
+    println(map.get("Testing"))
+
+    map.put("Testing", "Test")
+    println(map.get("Testing"))
+    println(map.get("Test"))
+    println(map.get("test"))
+
+    println("After Removing Xyz")
+    map.remove("Xyz")
+    println(map.get("Test"))
+    println(map.get("Testing"))
+
+    println("After Removing Test")
+    map.remove("Test")
+    println(map.get("Test"))
+    println(map.get("Testing"))
 }
