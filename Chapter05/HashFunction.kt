84c5679445460e8224729d460d9d3601970b95fa
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2018-08-16 23:27:43 +0530

    Added one more example for hashFunction.

--- Chapter05/HashFunction.kt
+++ Chapter05/HashFunction.kt
@@ -1,8 +1,19 @@
 fun hashCodeFromStudentId(studentId: String) = studentId[0].toUpperCase() - 'A'
 
+private fun hash(key: Char): Int {
+    var h = key.hashCode()
+    return h xor (h ushr 16)
+}
+
 fun main(args: Array<String>) {
     println(hashCodeFromStudentId("Apple"))
     println(hashCodeFromStudentId("angle"))
     println(hashCodeFromStudentId("Bangle"))
     println(hashCodeFromStudentId("ball"))
+
+    val alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    // Testing hash() function
+    for (i in 1..100) {
+        println("$i = ${hash(alphabets[i])}")
+    }
 }
