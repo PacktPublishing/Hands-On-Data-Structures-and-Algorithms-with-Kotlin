4adac23e3495736a884fc75af5e97e4b2c48d083
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2018-08-25 16:24:30 +0530

    Implemented all left APIs of ArrayMap.

--- Chapter05/ArrayMap.kt
+++ Chapter05/ArrayMap.kt
@@ -1,9 +1,7 @@
 import java.util.*
-import kotlin.math.*
 
 class ArrayMap<K, V> constructor(capacity: Int = 0) {
 
-    private val minCapacity = 4
     private lateinit var hashes: IntArray
     private lateinit var array: Array<Any?>
 
@@ -35,22 +33,80 @@ class ArrayMap<K, V> constructor(capacity: Int = 0) {
     fun valueAt(index: Int) = array[(index shl 1) + 1] as V
 
     fun setValueAt(index: Int, value: V): V? {
-        var index = index
-        index = (index shl 1) + 1
-        val old = array[index] as V
-        array[index] = value
+        val valueIndex = (index shl 1) + 1
+        val old = array[valueIndex] as V
+        array[valueIndex] = value
         return old
     }
 
     fun put(key: K, value: V): V? {
-        
+        val hash: Int
+        var index: Int
+        if (key == null) {
+            hash = 0
+            index = indexOfNull()
+        } else {
+            hash = key.hashCode()
+            index = indexOf(key)
+        }
+        if (index >= 0) {
+            // key-value pair aready present
+            index = (index shl 1) + 1
+            val old = array[index] as V
+            array[index] = value
+            return old
+        }
+
+        index = index.inv()
+        if (size >= hashes.size) {
+            val newSize = if (size < 4) 4 else size + (size shr 1)
+            val tempHashes = hashes
+            val tempArray = array
+            allocArrays(newSize)
+
+            System.arraycopy(tempHashes, 0, hashes, 0, tempHashes.size)
+            System.arraycopy(tempArray, 0, array, 0, tempArray.size)
+        }
+
+        if (index < size) {
+            System.arraycopy(hashes, index, hashes, index + 1, size - index)
+            System.arraycopy(array, index shl 1, array, (index + 1) shl 1, (size - index) shl 1)
+        }
+
+        hashes[index] = hash
+        array[index shl 1] = key
+        array[(index shl 1) + 1] = value
+        size++
+        return null
+    }
+
+    fun remove(key: K): V? {
+        val index = indexOfKey(key)
+        if (index >= 0) return removeAt(index)
+        return null
+    }
+
+    private fun removeAt(index: Int): V? {
+        val oldVal = array[(index shl 1) + 1]
+        val newSize = size - 1
+        if (size <= 1) {
+            // Empty Map
+            hashes = IntArray(0)
+            array = arrayOf()
+        } else {
+            System.arraycopy(hashes, index + 1, hashes, index, newSize - index)
+            System.arraycopy(array, (index + 1) shl 1, array, index shl 1, (newSize - index) shl 1)
+            array[newSize shl 1] = null
+            array[(newSize shl 1) + 1] = null
+        }
+        return oldVal as V
     }
 
     private fun indexOf(key: K): Int {
         val hash = key!!.hashCode()
 
         if (size == 0) return 0.inv()
-        val index = Arrays.binarySearch(hashes, hash)
+        val index = Arrays.binarySearch(hashes, 0, size, hash)
 
         // Key not found, return -ve value
         if (index < 0) return index
@@ -75,7 +131,7 @@ class ArrayMap<K, V> constructor(capacity: Int = 0) {
 
     private fun indexOfNull(): Int {
         if (size == 0) return 0.inv()
-        val index = Arrays.binarySearch(hashes, 0)
+        val index = Arrays.binarySearch(hashes, 0, size, 0)
 
         // Key not found, return -ve value
         if (index < 0) return index
@@ -98,12 +154,74 @@ class ArrayMap<K, V> constructor(capacity: Int = 0) {
         return end.inv()
     }
 
-    private fun allocArrays(capacity: Int) {
-        hashes = IntArray(capacity)
-        array = arrayOfNulls(capacity shl 1)   // capacity * 2
-    }    
+    private fun allocArrays(size: Int) {
+        hashes = IntArray(size)
+        array = arrayOfNulls(size shl 1)   // size * 2
+    }
 }
 
 fun main(args: Array<String>) {
-    
+    val map = ArrayMap<String, String>()
+    map.put("A", "Apple")
+    map.put("B", "Banana")
+    map.put("C", "Cucumber")
+
+    // Testing get
+    println(map.get("A"))
+    println(map.get("B"))
+    println(map.get("C"))
+    println(map.get("D"))
+
+    // Testing indexOfKey
+    println("Index Of Key - ${map.indexOfKey("A")}")
+    println("Index Of Key - ${map.indexOfKey("B")}")
+    println("Index Of Key - ${map.indexOfKey("C")}")
+    println("Index Of Key - ${map.indexOfKey("D")}")
+
+    // Testing containsKey
+    println("Contains Key - ${map.containsKey("A")}")
+    println("Contains Key - ${map.containsKey("B")}")
+    println("Contains Key - ${map.containsKey("C")}")
+    println("Contains Key - ${map.containsKey("D")}")
+
+    // Testing valueAt
+    println("Value At - ${map.valueAt(0)}")
+    println("Value At - ${map.valueAt(1)}")
+    println("Value At - ${map.valueAt(2)}")
+    println("Value At - ${map.valueAt(3)}")
+    try {
+        println("Value At - ${map.valueAt(8)}")
+    } catch(e: ArrayIndexOutOfBoundsException) {
+        e.printStackTrace()
+    }
+    try {
+        println("Value At - ${map.valueAt(-2)}")
+    } catch(e: ArrayIndexOutOfBoundsException) {
+        e.printStackTrace()
+    }
+
+    // Testing keyAt
+    println("Key At - ${map.keyAt(0)}")
+    println("Key At - ${map.keyAt(1)}")
+    println("Key At - ${map.keyAt(2)}")
+    println("Key At - ${map.keyAt(3)}")
+
+    // Testing setValueAt
+    map.setValueAt(1, "Beet")
+    println(map.get("A"))
+    println(map.get("B"))
+    println(map.get("C"))
+    println(map.get("D"))
+
+    // Testing remove
+    println(map.remove("B"))
+    println(map.get("A"))
+    println(map.get("B"))
+    println(map.get("C"))
+    println(map.get("D"))
+    map.put("B", "Banana")
+    println(map.get("A"))
+    println(map.get("B"))
+    println(map.get("C"))
+    println(map.get("D"))
 }
