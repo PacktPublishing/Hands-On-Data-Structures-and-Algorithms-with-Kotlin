81dd74a8967fd42deebf639773b66e4a1cc163ea
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2019-02-27 21:13:21 +0530

    Implemented quick sort in descending order and answered the corresponding question in assessment section too

--- Assessments/Chapter07/Answers.md
+++ Assessments/Chapter07/Answers.md
@@ -1,3 +1,62 @@
-#### Implement Quick Sort in descending order.
+#### 1. Implement Quick Sort in descending order.
 
-#### Implement all the sorting algorithms discussed above for immutable list.
+Below snippet is an implementation of sorting an array in descending order.
+
+```
+fun <E: Comparable<E>> Array<E>.descending() {
+    descending(this, 0, size - 1)
+}
+
+private fun <E: Comparable<E>> descending(arr: Array<E>, low: Int, high: Int) {
+    if (low < high) {
+        val partitionIndex = descendingPartition(arr, low, high)
+
+        descending(arr, low, partitionIndex - 1)
+        descending(arr, partitionIndex + 1, high)
+    }
+}
+
+private fun <E: Comparable<E>> descendingPartition(arr: Array<E>, low: Int, high: Int): Int {
+    val pivot = arr[high]
+    var i = low - 1
+    for (j in low until high) {
+        if (arr[j] >= pivot) {
+            i++
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+        }
+    }
+    arr[i + 1] = arr[high].also { arr[high] = arr[i + 1] }
+    return i + 1;
+}
+```
+
+Below snippet is an implementation of sorting an immutable list in descending order.
+```
+fun <E: Comparable<E>> MutableList<E>.descending() {
+    descending(this, 0, size - 1)
+}
+
+private fun <E: Comparable<E>> descending(arr: MutableList<E>, low: Int, high: Int) {
+    if (low < high) {
+        val partitionIndex = descendingPartition(arr, low, high)
+
+        descending(arr, low, partitionIndex - 1)
+        descending(arr, partitionIndex + 1, high)
+    }
+}
+
+private fun <E: Comparable<E>> descendingPartition(arr: MutableList<E>, low: Int, high: Int): Int {
+    val pivot = arr[high]
+    var i = low - 1
+    for (j in low until high) {
+        if (arr[j] >= pivot) {
+            i++
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+        }
+    }
+    arr[i + 1] = arr[high].also { arr[high] = arr[i + 1] }
+    return i + 1;
+}
+```
+
+#### 2. Implement all the sorting algorithms discussed above for immutable list.
--- Chapter07/QuickSort.kt
+++ Chapter07/QuickSort.kt
@@ -52,6 +52,58 @@ private fun <E: Comparable<E>> partition(arr: MutableList<E>, low: Int, high: In
     return i + 1;
 }
 
+fun <E: Comparable<E>> Array<E>.descending() {
+    descending(this, 0, size - 1)
+}
+
+private fun <E: Comparable<E>> descending(arr: Array<E>, low: Int, high: Int) {
+    if (low < high) {
+        val partitionIndex = descendingPartition(arr, low, high)
+
+        descending(arr, low, partitionIndex - 1)
+        descending(arr, partitionIndex + 1, high)
+    }
+}
+
+private fun <E: Comparable<E>> descendingPartition(arr: Array<E>, low: Int, high: Int): Int {
+    val pivot = arr[high]
+    var i = low - 1
+    for (j in low until high) {
+        if (arr[j] >= pivot) {
+            i++
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+        }
+    }
+    arr[i + 1] = arr[high].also { arr[high] = arr[i + 1] }
+    return i + 1;
+}
+
+fun <E: Comparable<E>> MutableList<E>.descending() {
+    descending(this, 0, size - 1)
+}
+
+private fun <E: Comparable<E>> descending(arr: MutableList<E>, low: Int, high: Int) {
+    if (low < high) {
+        val partitionIndex = descendingPartition(arr, low, high)
+
+        descending(arr, low, partitionIndex - 1)
+        descending(arr, partitionIndex + 1, high)
+    }
+}
+
+private fun <E: Comparable<E>> descendingPartition(arr: MutableList<E>, low: Int, high: Int): Int {
+    val pivot = arr[high]
+    var i = low - 1
+    for (j in low until high) {
+        if (arr[j] >= pivot) {
+            i++
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+        }
+    }
+    arr[i + 1] = arr[high].also { arr[high] = arr[i + 1] }
+    return i + 1;
+}
+
 fun main(args: Array<String>) {
     val nums = arrayOf(2, 12, 89, 23, 76, 43, 12)
     nums.sort()
@@ -64,4 +116,13 @@ fun main(args: Array<String>) {
     val languages = mutableListOf("Kotlin", "Java", "C#", "R", "Python", "Scala", "Groovy", "C", "C++")
     languages.sort()
     println(languages)
+
+    println()
+    println("Descending Order")
+    val numbers2 = arrayOf(2, 12, 89, 23, 76, 43, 12)
+    numbers2.descending()
+    println(Arrays.toString(numbers2))
+    val list = arrayOf("Kotlin", "Java", "C", "C++", "R", "Python", "Matlab")
+    list.descending()
+    println(Arrays.toString(list))
 }
