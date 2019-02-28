eededb99e20f3282acda2107e6bdbd1c65eefb6e
Chandra Sekhar Nayak <nayak.chandra1@gmail.com>
2019-02-28 14:38:27 +0530

    Implemented descending order sorting in bubble sort

--- Chapter07/BubbleSort.kt
+++ Chapter07/BubbleSort.kt
@@ -13,6 +13,19 @@ fun <E: Comparable<E>> Array<E>.sort() {
     }
 }
 
+fun <E: Comparable<E>> Array<E>.descending() {
+    val len = size
+    for (i in 0 until (len - 1)) {
+        for (j in 0 until (len - i - 1)) {
+            if (this[j].compareTo(this[j + 1]) < 0) {
+                val temp = this[j]
+                this[j] = this[j + 1]
+                this[j + 1] = temp
+            }
+        }
+    }
+}
+
 fun <E: Comparable<E>> MutableList<E>.sort() {
     val len = size
     for (i in 0 until (len - 1)) {
@@ -44,6 +57,8 @@ fun <E: Comparable<E>> List<E>.sort(): List<E> {
 fun main(args: Array<String>) {
     val nums = arrayOf(2, 12, 89, 23, 76, 43, 12)
     nums.sort()
+    println(Arrays.toString(nums))
+    nums.descending()
     println(Arrays.toString(nums))
 
     val languages = mutableListOf("Kotlin", "Java", "C#", "R", "Python", "Scala", "Groovy", "C", "C++")
